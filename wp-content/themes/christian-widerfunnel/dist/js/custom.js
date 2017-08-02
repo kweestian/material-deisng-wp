867dd5f0667a86918e759d5bdf279e7ebd832905
kweestian <chrislhamelin@gmail.com>
2017-08-02 13:27:36 -0700

    checking distro in

---
+++ wp-content/themes/christian-widerfunnel/dist/js/custom.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function ($) {
+ 
+  $('.parallax').parallax();
+  
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+
+ 
+ 
+});
