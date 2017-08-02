2833962ef02420028573c3fe2e19d588536f0043
kweestian <chrislhamelin@gmail.com>
2017-08-02 10:37:48 -0700

    material pagination

--- wp-content/themes/christian-widerfunnel/assets/js/custom/init.js
+++ wp-content/themes/christian-widerfunnel/assets/js/custom/init.js
@@ -1,5 +1,11 @@
-obj = {
-	foo: 'bar'
-}
-
-console.log(obj);
+jQuery(document).ready(function ($) {
+ 
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+ 
+ 
+});
